dce8bc6b028e64d1d829593447cd5c80110f8639
Jiajian Wu <jair.wu@rock-chips.com>
2024-10-24 19:11:54 +0800

    feat(drivers): support rkadk
    Signed-off-by: Jiajian Wu <jair.wu@rock-chips.com>

--- src/drivers/display/lv_display_common.h
+++ src/drivers/display/lv_display_common.h
@@ -29,6 +29,16 @@ void lv_drm_disp_set_overlay(lv_display_t * disp,
 #define lv_display_driver_create_overlay lv_drm_disp_create_overlay
 #define lv_display_driver_destroy_overlay lv_drm_disp_destroy_overlay
 #define lv_display_driver_set_overlay lv_drm_disp_set_overlay
+#elif defined(LV_USE_RKADK) && LV_USE_RKADK
+overlay_dma_buffer_t *lv_rkadk_disp_create_overlay(lv_display_t * disp,
+                                                   int w, int h);
+void lv_rkadk_disp_destroy_overlay(lv_display_t * disp,
+                                   overlay_dma_buffer_t * overlay);
+void lv_rkadk_disp_set_overlay(lv_display_t * disp,
+                               overlay_dma_buffer_t * overlay);
+#define lv_display_driver_create_overlay lv_rkadk_disp_create_overlay
+#define lv_display_driver_destroy_overlay lv_rkadk_disp_destroy_overlay
+#define lv_display_driver_set_overlay lv_rkadk_disp_set_overlay
 #endif
 
 #endif
