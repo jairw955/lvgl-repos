573a295f3c30ade033cb59d1c47229bf5501978c
Jiajian Wu <jair.wu@rock-chips.com>
2024-12-17 15:54:08 +0800

    fix(draw): improve sw_img
    1 Use fullscreen size buffer
    2 Use arithmetic shifts instead of multiplication
    
    Signed-off-by: Jiajian Wu <jair.wu@rock-chips.com>

--- src/draw/sw/lv_draw_sw_img.c
+++ src/draw/sw/lv_draw_sw_img.c
@@ -33,7 +33,7 @@
 /*********************
  *      DEFINES
  *********************/
-#define MAX_BUF_SIZE (uint32_t) (4 * lv_display_get_horizontal_resolution(lv_refr_get_disp_refreshing()) * lv_color_format_get_size(lv_display_get_color_format(lv_refr_get_disp_refreshing())))
+#define MAX_BUF_SIZE (uint32_t) (lv_display_get_vertical_resolution(lv_refr_get_disp_refreshing()) * lv_display_get_horizontal_resolution(lv_refr_get_disp_refreshing()) * lv_color_format_get_size(lv_display_get_color_format(lv_refr_get_disp_refreshing())))
 
 #ifndef LV_DRAW_SW_IMAGE
     #define LV_DRAW_SW_IMAGE(...)   LV_RESULT_INVALID
